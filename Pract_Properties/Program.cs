d0422384f36cc88495133aa1b6dcc7dbcc67f236
UkiOnna <sikirovtemirlan@mail.ru>
2017-12-04 17:00:51 +0600

    first

---
+++ Pract_Properties/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeOfArray array = new RangeOfArray();
+            Random random = new Random();
+            int rangeStart;
+            int rangeFinish;
+            Console.WriteLine("Введите диапозон массива");
+            int.TryParse(Console.ReadLine(), out rangeStart);
+            int.TryParse(Console.ReadLine(), out rangeFinish);
+            array.rangeStart = rangeStart;
+            array.rangeFinish = rangeFinish;
+            for(int i = rangeStart; i < rangeFinish; i++)
+            {
+                array[i] = random.Next(100);
+                Console.Write("{0}  ",array[i]);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Pract_Properties/RangeOfArray.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract_Properties
+{
+    class RangeOfArray
+    {
+        public int rangeStart { get; set; }
+        public int rangeFinish { get; set; }
+
+        private bool isRangeStart = false;
+        private int[] array;
+
+
+        public int this[int index]
+        {
+            get
+            {
+                if (rangeFinish < 0 && rangeStart < 0)
+                {
+                    if (!isRangeStart)
+                    {
+                        rangeStart = rangeStart * (-1);
+                        isRangeStart = true;
+                    }
+                    if (array == null)
+                    {
+                        array = new int[rangeStart - rangeFinish];
+                    }
+                    return array[rangeStart + index];
+                }
+                else
+                {
+                    if (!isRangeStart)
+                    {
+                        if (array == null)
+                        {
+                            array = new int[rangeFinish - rangeStart];
+                        }
+                        return array[index-rangeStart];
+                    }
+
+                    else
+                    {
+                        if (array == null)
+                        {
+                            array = new int[rangeStart - rangeFinish];
+                        }
+                        return array[rangeStart + index];
+                    }
+                }
+               
+               
+                    
+            }
+            set
+            {
+                if (rangeFinish < 0 && rangeStart < 0)
+                {
+                    if (!isRangeStart)
+                    {
+                        rangeStart = rangeStart * (-1);
+                        isRangeStart = true;
+                    }
+                    if (array == null)
+                    {
+                        array = new int[rangeStart - rangeFinish];
+                    }
+                    array[rangeStart + index] = value;
+                }
+                else
+                {
+                    if (!isRangeStart)
+                    {
+                        if (array == null)
+                        {
+                            array = new int[rangeFinish - rangeStart];
+                        }
+                        array[index-rangeStart] = value;
+                    }
+                    else
+                    {
+                        if (array == null)
+                        {
+                            array = new int[rangeStart - rangeFinish];
+                        }
+                        array[rangeStart + index] = value;
+                    }
+                }
+
+            }
+
+
+        }//индексирование
+    }
+}
